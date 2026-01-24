35d2eb1c68945af40f04c603801e6b78b602a595
adityamhjn <adityamhjn5@gmail.com>
2026-01-24 17:58:13 +0530

    Learned about different operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a and b)
+print(a or b)
+print(not (a > b))
+
