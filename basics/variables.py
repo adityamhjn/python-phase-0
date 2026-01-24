9a6d9fbd25de6f6a4234f7aae981098f033980b3
adityamhjn <adityamhjn5@gmail.com>
2026-01-24 17:43:38 +0530

    Learned about typecasting

--- basics/variables.py
+++ basics/variables.py
@@ -8,3 +8,8 @@ print("Name:", name)
 print("Age:", age)
 print("CGPA:", cgpa)
 print("Student:", is_student)
+# Type checking
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
