84acaadfb362d5362c55a6232fbeb2871b5298cc
adityamhjn <adityamhjn5@gmail.com>
2026-01-24 18:05:40 +0530

    learned about lists

---
+++ lists_basics.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.append(6)
+numbers.remove(3)
+
+print(numbers)
+print(len(numbers))
+print(numbers[0])
+print(numbers[-1])
+print(numbers[1:4])
+print(numbers.sort())
+print(numbers.reverse())
+print(numbers)  
