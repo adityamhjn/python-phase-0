585902eb7e0d6aee029f6015fee0a840df17e00f
adityamhjn <adityamhjn5@gmail.com>
2026-01-24 17:40:40 +0530

    Learned about loops

---
+++ basics/loops.py
@@ -0,0 +1,12 @@
+# Print first 10 natural numbers
+for i in range(1, 11):
+    print(i)
+
+# Sum of numbers in a list
+numbers = [10, 20, 30, 40]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("Sum:", total)
