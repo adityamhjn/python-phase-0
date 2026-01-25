a17348b4daf5ebbc6503ac90981360631784b971
adityamhjn <adityamhjn5@gmail.com>
2026-01-25 23:47:01 +0530

    Learned about dictionaries

---
+++ data_structures/dictionaries_basics.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Aman",
+    "marks": 85,
+    "branch": "CSE"
+}
+
+print(student["name"])
+print(student.get("marks"))
+student["marks"] = 90
+student["year"] = 2
+print(student)
