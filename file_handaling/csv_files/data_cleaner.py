34dd45198d058cbaaa917d0dbbd15208e4a41106
adityamhjn <adityamhjn5@gmail.com>
2026-01-31 22:14:58 +0530

    "Organized file_handling data by source and improved naming"

--- file_handaling/csv_files/data_cleaner.py
+++ file_handaling/csv_files/data_cleaner.py
@@ -8,7 +8,7 @@ with open("students.csv", "r") as file:
         if row.get("grade") != "":
             cleaned_rows.append(row)
 
-with open("cleaned_data.csv", "w", newline="") as file:
+with open("cleaned_students.csv", "w", newline="") as file:
     writer = csv.DictWriter(file, fieldnames=cleaned_rows[0].keys())
     writer.writeheader()
     writer.writerows(cleaned_rows)
--- file_handaling/json_files/read_json.py
+++ file_handaling/json_files/read_json.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), "../../students.json")
+file_path = os.path.join(os.path.dirname(__file__), "student.json")
 
 with open(file_path, "r") as file:
     data = json.load(file)
